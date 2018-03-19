f68ceef1aeea9bb3b2c6359937b2f43d9c74ce94
kanlav <kanlav@gmail.com>
2018-03-18 23:15:27 -0500

    this is my 3rd commit

---
+++ 02_package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"../stringUtil"
+)
+
+func main() {
+	fmt.Println(stringUtil.Myname)
+	fmt.Println(stringUtil.Reverse("My name"))
+}
---
+++ 02_package/stringUtil/name.go
@@ -0,0 +1,3 @@
+package stringUtil
+
+var Myname = "Udemy course"
